456cf206f351788d4d847799bb84e7a423960cbb
Study912020 <63300263+Study912020@users.noreply.github.com>
2020-09-30 18:52:22 +0530

    Update Convert_Model.bat

--- Face/Convert_Model.bat
+++ Face/Convert_Model.bat
@@ -1,4 +1,3 @@
-:: Copyright © 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 :: SPDX-License-Identifier: Apache-2.0
 call "C:\Program Files (x86)\IntelSWTools\openvino\bin\setupvars.bat"
 
