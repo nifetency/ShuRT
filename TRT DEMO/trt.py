20a1963071c57f7cf3ffa98922e745c45c0057e0
Arnol-d-Roy <69399762+Arnol-d-Roy@users.noreply.github.com>
2021-03-03 21:09:13 +0530

    Update trt.py

--- TRT DEMO/trt.py
+++ TRT DEMO/trt.py
@@ -39,49 +39,4 @@ time_my_model(mobilenet_v2, x)
 time_my_model(mobilenet_v2, x)
 time_my_model(mobilenet_v2, x)
 
-# Convert the SavedModel using TF-TRT
 
-# Prepare the configuration params for the conversion
-params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(
-    precision_mode='FP16',
-    is_dynamic_op=True)
-
-# Perform conversion
-converter = trt.TrtGraphConverterV2(
-    input_saved_model_dir='mobilenet_v2',
-    conversion_params=params)
-converter.convert()
-
-
-saved_model_dir_trt = 'mobilenet_v2.trt'
-converter.save(saved_model_dir_trt)
-
-root = tf.saved_model.load(saved_model_dir_trt)
-concrete_func = root.signatures['serving_default']
-
-concrete_func.structured_outputs
-
-labels_path = tf.keras.utils.get_file('ImageNetLabels.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
-imagenet_labels = np.array(open(labels_path).read().splitlines())
-
-labeling = concrete_func(tf.constant(x.astype('float32')))
-activations = tf.nn.softmax(labeling['predictions'])
-imagenet_labels[np.argsort(activations)[0,::-1][:5]+1]
-
-def time_trt_model():
-    image_input = tf.constant(x.astype('float32'))
-    times = []
-    for i in range(20):
-        start_time = time.time()
-        one_prediction = concrete_func(input_1=image_input)
-        delta = (time.time() - start_time)
-        times.append(delta)
-    mean_delta = np.array(times).mean()
-    fps = 1 / mean_delta
-    print('average(sec):{:.2f},fps:{:.2f}'.format(mean_delta, fps))
-
-time_trt_model()
-time_trt_model()
-time_trt_model()
-time_trt_model()
-time_trt_model()
